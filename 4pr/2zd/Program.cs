7d4f66c958f47bd2166c5f69e1d8654a9289fb68
Mindalka2569 <135545251+Mindalka2569@users.noreply.github.com>
2023-06-07 18:47:57 +0300

    Add files via upload

---
+++ 4pr/2zd/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2zd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bak = 1;
+            uint min;
+            Console.WriteLine("Введите минуты");
+            min = uint.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= min; i++)
+            {
+                bak = bak * 2;
+            }
+            Console.WriteLine("Количество бактерий " + bak);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 4pr/4pr/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4pr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите N");
+            int N;
+            N = Convert.ToInt32(Console.ReadLine());
+            double sum = 1;
+            double k = 1;
+
+            for (int i = 2; i <= N; i++)
+            {
+                k = sum / i + k;
+                Console.WriteLine(k);
+            }
+            Console.WriteLine("Сумма " + k);
+            Console.ReadKey();
+        }
+        
+    }
+}
